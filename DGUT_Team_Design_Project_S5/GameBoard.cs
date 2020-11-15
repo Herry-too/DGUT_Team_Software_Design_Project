45bc06f9b04651e583d8bc2ecb26fbe3be5589e6
Herry001 <21119203+Herry-too@users.noreply.github.com>
2020-11-15 17:22:48 +0800

    fix sln file disappear

--- DGUT_Team_Design_Project_S5/GameBoard.cs
+++ DGUT_Team_Design_Project_S5/GameBoard.cs
@@ -9,11 +9,19 @@ namespace DGUT_Team_Software_Project_Console
         String player = "red";
         public GameBoard()
         {
+
         }
 
         public void SwitchPlayer()
         {
-
+            if(player == "red")
+            {
+                player = "black";
+            }
+            else
+            {
+                player = "red";
+            }
         }
 
         public bool SelectPiece()
