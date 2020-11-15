3cdc6186f96510e6d76ad52621e7b6a7012a3b67
Siven <903393660@qq.com>
2020-11-15 18:44:20 +0800

    xiang

---
+++ DGUT_Team_Design_Project_S5/XiangPiece.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DGUT_Team_Software_Project_Console
+{
+    class XiangPiece : Piece
+    {
+        int intX;
+        int intY;
+        string player;
+        string Name;
+        public XiangPiece(string player, int intX, int intY): base (player,intX,intY)
+        {
+
+        }
+        
+        
+        public override bool ValidMoves(int x, int y, GameBoard gameboard, String player)
+        {
+            int[,] board = gameboard.board;
+            if (player != this.player)
+            {
+                return false;
+            }
+            if (x < 0 || x > 9)
+            {
+                return false;
+            }
+            if (y < 0 || y > 8)
+            {
+                return false;
+            }
+            if (x-intX == 2 || x - intX == -2)
+            {
+                if (y - intY == 2 || y - intY == -2)
+                {
+                    if(board[(x+intX)/2,(y+intY)/2]==0)//判断“田”字路径中间有没有子
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
